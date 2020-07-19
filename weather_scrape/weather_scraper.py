8d48405330c8a43f4cca577dd1e040efa2c2f2d5
Amtomkraftwerk <antonthielmann@t-online.de>
2020-07-19 19:51:40 +0200

    weatherscraper

---
+++ weather_scrape/weather_scraper.py
@@ -0,0 +1,55 @@
+import json
+import pandas as pd
+import webbot
+import time
+import bs4
+
+
+class WebScraper:
+    """
+    WebScraper
+    """
+
+    def __init__(self):
+        self.url = "https://www.wetter.com/wetter_aktuell/wettervorhersage/3_tagesvorhersage/deutschland/goettingen/DE0003197.html"
+        self.web = webbot.Browser()
+        self.web.go_to(self.url)
+        self.delay = 3
+        time.sleep(self.delay)
+        self.web.click("EINWILLIGEN")
+
+
+    def scrape(self):
+        time.sleep(self.delay)
+        page = self.web.get_page_source()
+        soup = bs4.BeautifulSoup(page, features='html.parser')
+        time.sleep(self.delay)
+        rains = soup.find_all('div', {'class': 'swg-col-wv1 swg-row'})
+        time.sleep(self.delay)
+        daytime = soup.find_all('div', {'class': 'swg-col-period swg-row'})
+        time.sleep(self.delay)
+        daytime_temp = soup.find_all('span', {'class': 'swg-text-large'})
+        time.sleep(self.delay)
+
+        daytime_list = []
+
+
+        for d, dt, r in zip(daytime, daytime_temp, rains):
+            daytime = d.text.strip()
+            time_temp = dt.text.strip()
+            rain = r.text.strip()
+
+            time_temp = time_temp.split('°')[0].strip()
+            rain = int(rain.strip("%\n "))
+            time_dict = {'daytime': daytime, 'temp': time_temp, 'rain': rain}
+            daytime_list.append(time_dict)
+
+        df = pd.DataFrame(daytime_list)
+        df['temp'] = df['temp'].astype(int)
+
+        return df
+
+if __name__ == "__main__":
+    ws = WebScraper()
+
+    df = ws.scrape()
