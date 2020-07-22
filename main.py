a3e0a7edaf23d485440e3fe64656a9652c199533
Amtomkraftwerk <antonthielmann@t-online.de>
2020-07-22 11:31:27 +0200

    commenting

--- main.py
+++ main.py
@@ -11,19 +11,21 @@ ws = WebScraper()
 es = EmailSender()
 
 # open login files for email sending
-with open("pkg/login.json", "r") as lg:
+with open("email_sender/login.json", "r") as lg:
     LG = json.load(lg)
 
 # create dataframe with temperature, days and rain
 df = ws.scrape()
 
 # create threshholds for good weather
-index_label_temp = set(df[df['temp']>=20].index.tolist())
-index_label_rain = df[df['rain']<=5].index.tolist()
+index_label_temp = set(df[df['temp'] >= 20].index.tolist())
+index_label_rain = df[df['rain'] <= 5].index.tolist()
 
 index = list(index_label_temp.intersection(index_label_rain))
 
 # define color coding
+
+
 def color_negative_red(val):
     """
     Takes a scalar and returns a string with
@@ -32,6 +34,7 @@ def color_negative_red(val):
     """
     color = 'green' if val <= 0.05 or val >= 18 else 'red'
     return 'background-color: %s' % color
+
 
 def color_blue(val):
     """
@@ -46,18 +49,18 @@ def color_blue(val):
 
 # color entries when weather is good
 df = df.style.applymap(color_negative_red,
-                  subset=pd.IndexSlice[index, ['temp', 'rain']])
+                       subset=pd.IndexSlice[index, ['temp', 'rain']])
 
 # mark the days
 df = df.applymap(color_blue,
-                      subset=pd.IndexSlice[[0, 5, 10], ['daytime', 'temp', 'rain']])
+                 subset=pd.IndexSlice[[0, 5, 10], ['daytime', 'temp', 'rain']])
 
 # save the file as excel sheet (colored)
 df.to_excel(writer, 'sheet1')
 writer.save()
 
 # send email when weather is good, otherwise not
-if (not index)==False:
+if (not index) == False:
     es.send(file="C:/Users/anton/Desktop/weather_search/output.xlsx")
 else:
     print('bad weather')
